22156a30ceb23d2f00ee7ec7282f51633b370d92
Dhruv Madaan <madaandhruv50@gmail.com>
2024-08-28 20:10:08 +0530

    changed the cors settings

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import cookieParser from 'cookie-parser';
 const app = express();
 app.use(cors({
     credentials: true,
-    origin: "https://66cf32bb5ca7217778cd6bbc--super-rolypoly-d789b4.netlify.app/"
+    origin: "https://super-rolypoly-d789b4.netlify.app"
 }))
 app.use(cookieParser());
 app.use(express.json({ limit: "16kb" }));
